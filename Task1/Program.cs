0b821cd010da9976a0f0d24185c3f86004ef3c30
Nikita <varashilov125@gmail.com>
2023-06-30 15:05:06 +0300

    Task2 число Фибоначи + осваиваем void

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4*/
+
+
+int[] arrayNumbers;
+arrayNumbers = GetArray();
+
+Console.WriteLine($"Числа введенные пользователем = [{String.Join(", ", arrayNumbers)}]");
+Console.WriteLine($"Чисел больше нуля = {GetNumbersGreaterThanZero(arrayNumbers)}");
+
+int[] GetArray()//Функция получения массива, вводимый пользователем
+{
+    Console.Write("Введите числа через пробел: ");
+    string stringNumbers = Console.ReadLine()!;//"87 25 32 155 1"
+    string[] stringsArray = stringNumbers.Split(' '); //"87 25 32 155 1" => ["87", "25", "32", "155", "1"]
+
+    int[] arrayNumbers = new int[stringsArray.Length];
+    for (var i = 0; i < arrayNumbers.Length; i++)
+    {
+        arrayNumbers[i] = int.Parse(stringsArray[i]);
+    }
+    return arrayNumbers;
+}
+
+int GetNumbersGreaterThanZero(int[] array)//Получение кол-ва чисел больше нуля
+{
+    int NumbersGreaterThanZero = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            NumbersGreaterThanZero += 1;
+        }
+    }
+    return NumbersGreaterThanZero;
+}
